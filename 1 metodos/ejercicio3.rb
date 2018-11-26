3a38b3ac9050af41c7cef9fb35597871396f07a2
aleion <alejandro.ion.carvajal@gmail.com>
2018-11-26 16:29:19 -0300

    arreglo ejercicio 3

--- 1 metodos/ejercicio3.rb
+++ 1 metodos/ejercicio3.rb
@@ -2,11 +2,11 @@
 # se le pase un numero mayor a 5 y false en caso contrario.
 
 def check5(num)
-  if num > 5
+  if num >= 5
     puts true
   elsif puts false
   end
 end
 
-check5(3) # Deberia ser false
-check5(6) # Deberia ser true
+check5(5) # Deberia ser false
+check5(2) # Deberia ser true
