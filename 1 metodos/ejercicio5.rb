73ab9af10b1261a79262f8007f15faa26b2a9fb4
aleion <alejandro.ion.carvajal@gmail.com>
2018-11-26 16:35:40 -0300

    ejercicio 5 listo y limip

--- 1 metodos/ejercicio5.rb
+++ 1 metodos/ejercicio5.rb
@@ -1,2 +1,10 @@
-# Crear un método que reciba como parámetro dos números enteros positivos
-# e imprima los números pares que existen entre esos dos números.
+# Crear un metodo que reciba como parametro dos numeros enteros positivos
+# e imprima los numeros pares que existen entre esos dos numeros.
+
+def pares(num1, num2)
+  (num1..num2).each do |i|
+    puts i if i.even?
+  end
+end
+
+pares(2, 11)
