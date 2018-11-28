909de7ef3c10cb88a098e772e93a13550a8627e3
aleion <alejandro.ion.carvajal@gmail.com>
2018-11-28 15:59:16 -0300

    Metodos - Ejercicio 1,3,4,5 OK

--- 1 metodos/ejercicio5.rb
+++ 1 metodos/ejercicio5.rb
@@ -2,9 +2,9 @@
 # e imprima los numeros pares que existen entre esos dos numeros.
 
 def pares(num1, num2)
-  (num1..num2).each do |i|
-    puts i if i.even?
+  (num1..num2).each do |par|
+    puts par if par.even?
   end
 end
 
-pares(23, 54)
+pares(2, 18)
